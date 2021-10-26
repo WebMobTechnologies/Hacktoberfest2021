791a1f32b5aae968822f32a0d151a68966e38697
Nikhil Verma <nick20899@gmail.com>
2021-10-26 17:44:52 +0530

    added demo card

--- Recipe App/recipe-app/src/App.js
+++ Recipe App/recipe-app/src/App.js
@@ -1,11 +1,14 @@
 import "./App.scss";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
+import Recipes from "./components/Recipes";
 function App() {
   return (
     <div className="App">
       <Header />
-      <div className="mainContent"></div>
+      <div className="mainContent">
+        <Recipes />
+      </div>
       <Footer />
     </div>
   );
