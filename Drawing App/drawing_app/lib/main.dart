6e09368f3866de45024247495cf8b9900414b47d
RAJ CHOKSHI <rajchokshi707@gmail.com>
2021-10-27 20:30:02 +0530

    Added Pen Working and Figure Save Code

--- Drawing App/drawing_app/lib/main.dart
+++ Drawing App/drawing_app/lib/main.dart
@@ -41,7 +41,29 @@ class _DrawAppState extends State<DrawApp> {
   //key
   GlobalKey globalKey = GlobalKey();
   //uuid generator generates unique id
-  // Uuid uuid = Uuid();
+   Uuid uuid = Uuid();
+
+
+  //image gallery saver
+  Future<void> _save() async {
+    try{
+      RenderRepaintBoundary boundary =
+      globalKey.currentContext!.findRenderObject() as RenderRepaintBoundary;
+      ui.Image image = await boundary.toImage();
+      ByteData? byteData =
+      await image.toByteData(format: ui.ImageByteFormat.png);
+      Uint8List pngBytes = byteData!.buffer.asUint8List();
+      // print("Png Bytessssss" + "  "+pngBytes.toString());
+
+      final result = await ImageGallerySaver.saveImage(
+          Uint8List.fromList(pngBytes),
+          quality: 60,
+          name: "${uuid.v1().toString()}");
+      print(result);
+    }catch(e){
+      print("Errorrrrrrrrr" + "  " + e.toString() );
+    }
+  }
 
   @override
   void initState() {
@@ -92,7 +114,7 @@ class _DrawAppState extends State<DrawApp> {
         children: [
           Container(
             decoration: BoxDecoration(
-              color: Colors.black,
+                color: color // color of the main container
             ),
           ),
           Center(
@@ -100,28 +122,49 @@ class _DrawAppState extends State<DrawApp> {
               mainAxisAlignment: MainAxisAlignment.center,
               crossAxisAlignment: CrossAxisAlignment.center,
               children: [
+
                 Container(
+                  width: MediaQuery.of(context).size.width * 0.80,
+                  height: MediaQuery.of(context).size.height * 0.80,
                   decoration: BoxDecoration(
-                    borderRadius: BorderRadius.all(Radius.circular(20.0)),
-                    boxShadow: [
-                      BoxShadow(
-                          color: Colors.black.withOpacity(0.4),
-                          // color of the  box shadow
-                          blurRadius: 5.0,
-                          spreadRadius: 1.0)
-                    ],
+                      borderRadius: BorderRadius.all(Radius.circular(20.0)),
+                      boxShadow: [
+                        BoxShadow(
+                            color: Colors.black.withOpacity(0.4),// color of the  box shadow
+                            blurRadius: 5.0,
+                            spreadRadius: 1.0
+                        )
+                      ]
                   ),
                   child: GestureDetector(
-                    onPanDown: (details) {
-                      setState(() {
-
+                    onPanDown: (details){
+                      this.setState(() {
+                        points.add(DrawingArea(
+                            point: details.localPosition,
+                            areaPaint: Paint()
+                              ..strokeCap = StrokeCap.round
+                              ..isAntiAlias = true
+                              ..color = color!
+                              ..strokeWidth = strokeWidth
+                        ));
                       });
                     },
-                    onPanUpdate: (details) {
-                      setState(() {});
+                    onPanUpdate: (details){
+                      this.setState(() {
+                        points.add(DrawingArea(
+                            point: details.localPosition,
+                            areaPaint: Paint()
+                              ..strokeCap = StrokeCap.round
+                              ..isAntiAlias = true
+                              ..color = color!
+                              ..strokeWidth = strokeWidth
+                        ));
+                      });
                     },
-                    onPanEnd: (details) {
-                      setState(() {});
+                    onPanEnd: (details){
+                      this.setState(() {
+                        points.add(null);
+                      });
                     },
                     child: RepaintBoundary(
                       key: globalKey,
@@ -140,7 +183,8 @@ class _DrawAppState extends State<DrawApp> {
                 Container(
                   decoration: BoxDecoration(
                       color: Colors.white,
-                      borderRadius: BorderRadius.circular(20.0)),
+                      borderRadius: BorderRadius.circular(20.0)
+                  ),
                   width: MediaQuery.of(context).size.width * 0.80,
                   child: Row(
                     mainAxisAlignment: MainAxisAlignment.center,
@@ -149,8 +193,10 @@ class _DrawAppState extends State<DrawApp> {
                         child: Slider(
                           min: 1.0,
                           max: 5.0,
+
                           activeColor: color,
                           value: strokeWidth,
+
                           onChanged: (double value) {
                             this.setState(() {
                               strokeWidth = value;
@@ -158,36 +204,27 @@ class _DrawAppState extends State<DrawApp> {
                           },
                         ),
                       ),
-                      IconButton(
-                          onPressed: () {
-                            setState(() {
-                              points.clear();
-                            });
-                          },
-                          icon: Icon(
-                            Icons.phonelink_erase_rounded,
-                            color: color,
-                          )),
-                      IconButton(
-                          onPressed: () {
-                            pickColor(context);
-                          },
-                          icon: Icon(
-                            Icons.color_lens,
-                            color: color,
-                          )),
-                      IconButton(
-                          onPressed: () {
-                            ScaffoldMessenger.of(context).showSnackBar(
-                                SnackBar(content: Text("Button pressed")));
-                            setState(() {
-                              // _save();
-                            });
-                          },
-                          icon: Icon(Icons.save))
+
+
+                      IconButton(onPressed: (){
+                        setState(() {
+                          points.clear();
+                        });
+
+                      }, icon: Icon(Icons.phonelink_erase_rounded,color: color,)),
+                      IconButton(onPressed: (){
+                        pickColor(context);
+
+                      }, icon: Icon(Icons.color_lens,color: color,)),
+                      IconButton(onPressed: (){
+                        ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text("Button pressed")));
+                        setState(() {
+                          _save();
+                        });
+                      }, icon: Icon(Icons.save))
                     ],
                   ),
-                ),
+                )
               ],
             ),
           ),
